4dc7df38eb34a41c4f72673ba57e51910c4f94a2
Henrique Rodrigues <ricknet10@gmail.com>
2023-05-08 12:00:34 -0300

    Seller Controller

---
+++ AplicativoVendas/Controllers/SellerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AplicativoVendas.Controllers
+{
+    public class SellerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
